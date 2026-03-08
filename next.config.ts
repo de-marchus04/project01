f1e7cf56e947088455facdeabbb6dffe59b43581
Dmitrij <derzhavin.dm@gmail.com>
2026-03-08 19:35:25 +0100

    debug: temporarily disable sentry wrapper to test auth route

--- next.config.ts
+++ next.config.ts
@@ -50,7 +50,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default withSentryConfig(withBundleAnalyzer(nextConfig), {
-  silent: true,
-  disableLogger: true,
-});
+export default withBundleAnalyzer(nextConfig);
